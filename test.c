586b2f74ccd2b14884582a53d274dd2b196ddddc
fengjunhuii <993935340@qq.com>
2019-07-16 16:06:32 +0800

    add test.c file

---
+++ test.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	#	 FileName	: test.c
+	#	 Author		: fengjunhui 
+	#	 Email		: 18883765905@163.com 
+	#	 Created	: 2019年07月16日 星期二 16时04分51秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("hello github.\n");
+
+    return 0;
+}
